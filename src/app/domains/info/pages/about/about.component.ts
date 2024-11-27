e0eaeaf453c4eb12abff84bfc1208a6193a87513
Juan Higuita <jhiguitac@gmail.com>
2024-11-26 22:44:21 -0500

    Se despliega aplicación en la nube de vercel

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CounterComponent } from '../../../shared/components/counter/counter.component';
-import { WaveAudioComponent } from '../../../info/components/wave-audio/wave-audio.component';
+import { CounterComponent } from '@shared/components/counter/counter.component';
+import { HighlightDirective } from '@shared/directives/highlight.directive'
+import { WaveAudioComponent } from '@info/components/wave-audio/wave-audio.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CounterComponent, CommonModule, WaveAudioComponent],
+  imports: [CounterComponent, CommonModule, WaveAudioComponent, HighlightDirective],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
