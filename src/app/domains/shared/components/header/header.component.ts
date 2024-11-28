b44f0a46cdee2e6fb813815879c125088b0006ca
Juan Higuita <jhiguitac@gmail.com>
2024-11-27 22:50:57 -0500

    Se crea página 404 y se incluye header en about

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, Input, signal, SimpleChanges } from '@angular/core';
 import { CommonModule, CurrencyPipe } from '@angular/common';
 import { CartService } from '../../services/cart.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, CurrencyPipe],
+  imports: [CommonModule, CurrencyPipe, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
