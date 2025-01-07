fee50777b15d0c42d442546bb6098adc36a5dfc1
Juan Higuita <jhiguitac@gmail.com>
2025-01-07 12:36:25 -0500

    Se implementa consulta de productor por categoria

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,18 +1,18 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, Input, signal, SimpleChanges } from '@angular/core';
 import { ProductComponent } from '@products/components/product/product.component';
 import { CommonModule } from '@angular/common';
 import { Product } from '@shared/models/product.model';
-import { HeaderComponent } from "@shared/components/header/header.component";
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
 import { CategoryService } from '@shared/services/category.service';
 import { Category } from '@shared/models/category.model';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [ProductComponent, CommonModule],
+  imports: [ProductComponent, CommonModule, RouterLink],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
@@ -20,14 +20,18 @@ import { Category } from '@shared/models/category.model';
 export class ListComponent {
   private cartService = inject(CartService);
   private productService = inject(ProductService);
-  private categoryService = inject(CategoryService)
+  private categoryService = inject(CategoryService);
+  @Input() category_id?: string;
 
   products = signal<Product[]>([]);
   categories = signal<Category[]>([]);
   
   ngOnInit(){
-    this.getProducts();
     this.getCategories();
+  }
+
+  ngOnchanges(changes: SimpleChanges){ 
+    this.getProducts();
   }
 
   addtoCart(product: Product){
@@ -35,7 +39,7 @@ export class ListComponent {
   }
 
   private getProducts(){
-    this.productService.getProducts().subscribe({
+    this.productService.getProducts(this.category_id).subscribe({
       next: (products) => {
         this.products.set(products);
 
